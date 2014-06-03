6713cbbb04f554c8451ec5a74c1dc6ee294bae17
Erik James Albaugh <erik@maintainweb.co>
2014-06-03 08:17:00 -0700

    Styles & Scripts

--- lib/scripts.php
+++ lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '288a04d5ae1f483e993399ffff5a6af0');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, 'd73d18db82d320880ddeca291cd2b4ca');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
