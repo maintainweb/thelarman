17f9eee452f73f8084c013ed41d6188e7a467295
Erik James Albaugh <erik@maintainweb.co>
2014-06-04 17:23:08 -0700

    Remove auto formatting of telephone
    Using a meta tag to remove formatting of the telephone.

--- templates/head.php
+++ templates/head.php
@@ -5,6 +5,7 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title><?php wp_title('|', true, 'right'); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="format-detection" content="telephone=no">
 
   <?php wp_head(); ?>
 
